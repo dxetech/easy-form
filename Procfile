c51c97bbc5e78f3cc2a65ce7bc7606110c62b299
James Maxwell <james@dxetech.com>
2013-02-25 20:19:22 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+api: venv/bin/gunicorn -w 1 -b 127.0.0.1:8082 easyform.wsgi:application
---
+++ emailer/views.py
@@ -0,0 +1,47 @@
+import json
+from boto import ses
+from django.http import HttpResponse
+from easyform.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_KEY
+
+# Create your views here.
+def json_response(func):
+    """
+    A decorator thats takes a view response and turns it
+    into json. If a callback is added through GET or POST
+    the response is JSONP.
+    """
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = json.dumps(objects)
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+            data = json.dumps(str(objects))
+        return HttpResponse(data, "application/json")
+    return decorator
+
+@json_response
+def home(request):
+    return {"message" : "index"}
+
+@json_response
+def mailer(request):
+    for k in ["name", "email", "subject", "body", "to_addresses"]:
+        if k not in request.REQUEST:
+            return {'message' : 'ERROR, param %s missing' % k}
+    connection = ses.connection.SESConnection(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY)
+    body = "Email From: %s, %s \n%s" % (request.REQUEST["name"],
+            request.REQUEST["email"], request.REQUEST["body"])
+    email_context = {
+        "source" : "james@dxetech.com",
+        "subject" : request.REQUEST["subject"],
+        "body" : body,
+        "to_addresses" : request.REQUEST["to_addresses"]
+        }
+    connection.send_email(**email_context)
+    return {'message' : 'successful'}
