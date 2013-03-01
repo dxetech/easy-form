d29208343730a88f5e3705ae4b29e840c3b9fecd
James Maxwell <james@dxetech.com>
2013-03-01 14:40:58 +0000

    added source

--- emailer/views.py
+++ emailer/views.py
@@ -36,14 +36,14 @@ def mailer(request):
     api_key = request.REQUEST.get("api_key", "")
     key = get_object_or_404(Key, api_key=api_key)
 
-    for k in ["name", "email", "subject", "body", "to_addresses"]:
+    for k in ["name", "email", "subject", "body", "to_addresses", "source"]:
         if k not in request.REQUEST:
             return {'message' : 'ERROR, param %s missing' % k}
     connection = ses.connection.SESConnection(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY)
     body = "Email From: %s, %s \n%s" % (request.REQUEST["name"],
             request.REQUEST["email"], request.REQUEST["body"])
     email_context = {
-        "source" : "james@dxetech.com",
+        "source" : request.REQUEST["source"],
         "subject" : request.REQUEST["subject"],
         "body" : body,
         "to_addresses" : request.REQUEST["to_addresses"]
